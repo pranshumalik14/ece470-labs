3555f8ae4eba743eef52639af4e5813ca077a174
varunsampat30 <varunsampat30@gmail.com>
2022-02-25 15:23:57 -0500

    Delta joint and mykuka search defined

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -7,25 +7,29 @@ clc;
 close all;
 clearvars -except udpObj;
 
-%% Prelab : set up DH Matrix for the KUKA, forward and inverse kinematics
+% DH Matrix for the KUKA, forward and inverse kinematics
 % in order of theta_i, d_i, a_i, alpha_i
-
-% syms theta_1 theta_2 theta_3 theta_4 theta_5 theta_6
 thetas = zeros(6); % initialize all thetas to zero
 
-% Note a and d values are in [m] and not [mm]
+% note values are SI units (rad, meters)
+link_1 = [thetas(1)  0.400    0.025      pi/2 ];
+link_2 = [thetas(2)  0        0.315      0    ];
+link_3 = [thetas(3)  0        0.035      pi/2 ];
+link_4 = [thetas(4)  0.365    0          -pi/2];
+link_5 = [thetas(5)  0        0          pi/2 ];
+link_6 = [thetas(6)  0.16144  -0.29633   0   ];
+DH     = [link_1; link_2; link_3; link_4; link_5; link_6];
 
-link_1 = [thetas(1)  0.400    0.025        pi/2 ];
-link_2 = [thetas(2)  0        0.315        0    ];
-link_3 = [thetas(3)  0        0.035        pi/2 ];
-link_4 = [thetas(4)  0.365    0            -pi/2];
-link_5 = [thetas(5)  0        0            pi/2 ];
-link_6 = [thetas(6)  0.16144  -0.29633     0    ];
+% create the KUKA robot model and test FK and IK for a given joint config
+kuka    = mykuka(DH);
+fk_test = forward_kuka([pi/5 pi/3 -pi/4 pi/4 pi/3 pi/4]', kuka)
+ik_test = inverse_kuka(fk_test, kuka)
 
-DH = [link_1; link_2; link_3; link_4; link_5; link_6];
 
-% create the KUKA robot model
-kuka   = mykuka(DH);
-fk_ans = forward_kuka([pi/5 pi/3 -pi/4 pi/4 pi/3 pi/4]', kuka)
+%% part 4.3: calibrate DH table or robot model
 
-ik_ans = inverse_kuka(fk_ans, kuka)
+% minimize calibration error
+optdelta = fminunc(@deltajoint, [0 0])
+
+% redefine robot using updated DH
+myrobot = mykuka_search(optdelta)
---
+++ lab2/mykuka_search.m
@@ -0,0 +1,19 @@
+% returns the kuka robot with a parametric DH table
+function kuka = mykuka_search(delta)
+
+% DH Matrix for the KUKA, forward and inverse kinematics
+% in order of theta_i, d_i, a_i, alpha_i
+thetas = zeros(6); % initialize all thetas to zero
+
+% note values are SI units (rad, meters)
+link_1 = [thetas(1)  0.400             0.025              pi/2 ];
+link_2 = [thetas(2)  0                 0.315              0    ];
+link_3 = [thetas(3)  0                 0.035              pi/2 ];
+link_4 = [thetas(4)  0.365             0                  -pi/2];
+link_5 = [thetas(5)  0                 0                  pi/2 ];
+link_6 = [thetas(6)  0.16144+delta(1) -0.29633+delta(2)   0    ];
+DH     = [link_1; link_2; link_3; link_4; link_5; link_6];
+
+kuka = mykuka(DH)
+
+end
